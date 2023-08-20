7a29007cfce9814d4ac998e66e05eb7941bf5b19
placeparks <placeparks@gmail.com>
2023-08-20 08:44:04 -0700

    first commit

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,138 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@thirdweb-dev/contracts/base/ERC721Base.sol";
 
 contract MyContract {
-    constructor() {}
+    address public owner;
+    mapping(address => uint256) public entryCount;
+    address[] public players;
+    address[] private playerSelector;
+    bool public raffleStatus;
+    uint256 public entryCost;
+    address public nftAddress;
+    uint256 public nftId;
+    uint256 public totalEntries;
+
+    event NewEntry(address player);
+    event WinnerSelected(address winner);
+    event RaffleStarted();
+    event RaffleEnded();
+    event EntryCostChanged(uint256 newCost);
+    event NFTPrizeSet(address nftAddress, uint256 nftId);
+    event BalanceWithdrawn(uint256 amount);
+
+    constructor(uint256 _entryCost) {
+        owner = msg.sender;
+        entryCost = _entryCost; 
+        raffleStatus = false;
+        totalEntries = 0;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
+    function startRaffle(address _nftContract, uint256 _tokenID) public onlyOwner {
+        require(!raffleStatus, "Raffle is already started");
+        require(nftAddress == address(0), "NFT prize is already set");
+        require(
+            ERC721Base(_nftContract).ownerOf(_tokenID) == owner,
+            "Owner does not own this NFT"
+        );
+        nftAddress = _nftContract;
+        nftId = _tokenID;
+        raffleStatus = true;
+        emit RaffleStarted();
+        emit NFTPrizeSet(_nftContract, _tokenID); 
+    }
+
+function buyEntry(uint256 _numberOfEntries) public payable{
+    require(raffleStatus, "Raffle is not started");
+    require(msg.value == entryCost * _numberOfEntries, "Incorrect amount sent");
+    entryCount[msg.sender] += _numberOfEntries; 
+    totalEntries += _numberOfEntries;
+    if(!isPlayer(msg.sender)){
+        players.push(msg.sender);
+    }
+    for(uint256 i = 0; i < _numberOfEntries; i++){
+        playerSelector.push(msg.sender);
+    }
+    emit NewEntry(msg.sender);
+
+}
+
+function isPlayer(address _player) public view returns(bool){
+    for(uint256 i = 0; i < players.length; i++){
+        if(players[i] == _player){
+            return true;
+        }
+    }
+    return false;
+}
+
+function endRaffle() public onlyOwner{
+    require(raffleStatus, "Raffle is not started");
+ 
+    raffleStatus = false;
+  emit RaffleEnded();
+}
+
+function selectWinner() public onlyOwner{
+    require(!raffleStatus, "Raffle is not ended");
+    require(playerSelector.length > 0, "No players in raffle");
+    require(nftAddress != address(0), "NFT prize is not set");
+    uint256 winnerIndex = random() % playerSelector.length;
+    address winner = playerSelector[winnerIndex];
+    emit WinnerSelected(winner);
+        ERC721Base(nftAddress).safeTransferFrom(owner, winner, nftId);
+resetEntryCounts();
+delete playerSelector;
+delete players;
+nftAddress = address(0);
+nftId = 0;
+totalEntries = 0;
+}
+
+function random() private view returns(uint256){
+    return uint256(keccak256(abi.encodePacked(
+        block.prevrandao, block.timestamp, players.length)));
+
+}
+
+function resetEntryCounts() private{
+    for(uint256 i = 0; i < players.length; i++){
+        entryCount[players[i]] = 0;
+    }
+}
+
+function changeEntryCost(uint256 _newCost) public onlyOwner{
+    require(!raffleStatus, "Raffle is not ended");
+    entryCost = _newCost;
+    emit EntryCostChanged(_newCost);
+}
+function withdrawBalance() public onlyOwner{
+    require(address(this).balance > 0, "No balance to withdraw");
+    uint256 balanceAmount = address(this).balance;
+    payable(owner).transfer(balanceAmount);
+    emit BalanceWithdrawn(balanceAmount);
+}
+
+function getPlayers() public view returns(address[] memory){
+    return players;
+}
+
+function getBalance() public view returns(uint256){
+    return address(this).balance;   
+}
+
+function resetsContract() public onlyOwner{
+delete players;
+delete playerSelector;
+nftAddress = address(0);
+nftId = 0;
+totalEntries = 0;
+raffleStatus = false;
+entryCost = 0;
+resetEntryCounts();
+}
 }
